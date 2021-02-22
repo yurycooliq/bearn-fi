7128414e8e6c49db24f159802ab28291c675b4fb
Yury Cooliq <yurycooliq@gmail.com>
2021-02-22 03:04:49 +0200

    Pretty

--- src/start.js
+++ src/start.js
@@ -38,19 +38,19 @@ const flashLog = async lpTokensArray => {
 
     Promise.all(promises).then(values => {
         const p = new Table({
-            title: `bEarn.fi Portfolio Tracker at ${(new Date).toLocaleTimeString()}`,
+            title: `bEarn.fi Portfolio Tracker for ${process.env.USER_ADDRESS}`,
             columns: [
-                { name: "name",             title: "Pair",              alignment: "left" ,     color: "cyan"  },
-                { name: "token1_price",     title: "Token 1 USD",       alignment: "right",     color: "green" },
-                { name: "token2_price",     title: "Token 2 USD",       alignment: "right",     color: "green" },
-                { name: "lp_token_price",   title: "LP token USD",      alignment: "right",     color: "green" },
-                { name: "TVL",              title: "TVL*",              alignment: "right",     color: "green" },
-                { name: "lp_amount",        title: "LP Amount",         alignment: "right",     color: "green" },
-                { name: "bdo_reward",       title: "BDO Reward",        alignment: "right",     color: "green" },
-                { name: "lp_cost",          title: "LP Cost",           alignment: "right",     color: "green" },
-                { name: "profit",           title: "Profit",            alignment: "right",     color: "white_bold" }
+                { name: "name",           title: "Vaults",     alignment: "left",  color: "cyan" },
+                { name: "token1_price",   title: "Token 1",    alignment: "right", color: "green" },
+                { name: "token2_price",   title: "Token 2",    alignment: "right", color: "green" },
+                { name: "lp_token_price", title: "LP Cost",    alignment: "right", color: "green" },
+                { name: "lp_amount",      title: "LP Amount",  alignment: "right", color: "green" },
+                { name: "TVL",            title: "TVL*",       alignment: "right", color: "green" },
+                { name: "bdo_reward",     title: "BDO Reward", alignment: "right", color: "green" },
+                { name: "lp_cost",        title: "Deposit",    alignment: "right", color: "green" },
+                { name: "profit",         title: "Profit",     alignment: "right", color: "white_bold" }
             ],
-            // filter: row => +row.lp_amount > 0
+            filter: row => +row.lp_amount > 0
         })
 
         p.addRows(values)
@@ -58,11 +58,12 @@ const flashLog = async lpTokensArray => {
         console.clear()
         p.printTable()
 
-        console.log("* On PancakeSwap for pairs and CafeSwap for BREW\n")
+        console.log(" * On PancakeSwap for pairs and CafeSwap for BREW\n")
 
-        console.log("URL:", "https://bearn.fi/bvaults") // Clickable in most consoles
-        console.log(`Total LP Cost: ${sumDollars(values, "lp_cost")}`)
-        console.log(`Total Profit: ${sumDollars(values, "profit")}`)
+        console.log(` Total Deposit: ${sumDollars(values, "lp_cost")}`)
+        console.log(`  Total Profit: ${sumDollars(values, "profit")}`)
+        console.log(` Last Snapshot: ${(new Date).toLocaleTimeString()}`)
+        console.log("           URL: https://bearn.fi/bvaults") // Clickable in most consoles
     }).catch(console.log)
 }
 
